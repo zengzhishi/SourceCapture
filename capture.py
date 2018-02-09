f9f2aa99b4d0f7ff5ae605a8cc8941a5d716c5fb
zengzhishi <zengzhishi@sbrella.com>
2018-02-09 20:25:01 +0800

    fix develop and master conflict.

--- capture.py
+++ capture.py
@@ -11,10 +11,7 @@
 
 import os
 import sys
-<<<<<<< HEAD
 import time
-=======
->>>>>>> develop
 import re
 import conf.parse_logger as parse_logger
 import source_detective as source_detective
@@ -36,20 +33,6 @@ except ImportError:
 DEFAULT_LOG_CONFIG_FILE = "conf/logging.conf"
 DEFAULT_COMPILE_COMMAND = "gcc"
 
-<<<<<<< HEAD
-=======
-COMPILER_COMMAND_MAP = {
-    "GNU": {
-        "CXX": "g++",
-        "C": "gcc"
-    },
-    "Clang": {
-        "CXX": "clang++",
-        "C": "clang"
-    }
-}
-
->>>>>>> develop
 
 # 并发构建编译命令
 class CommandBuilder(building_process.ProcessBuilder):
@@ -91,40 +74,11 @@ class CommandBuilder(building_process.ProcessBuilder):
             args_string = flag_string + definition_string + include_string
 
             sources = job_dict["source_files"]
-<<<<<<< HEAD
             for src in sources:
                 transfer_name = file_args_MD5Calc(src, \
                             job_dict["flags"], job_dict["definitions"], job_dict["includes"])
                 output_command = self.compile_command[job_dict["compiler_type"]] + " "
                 output_command += args_string
-=======
-            custom_flags = job_dict["custom_flags"]
-            custom_definitions = job_dict["custom_definitions"]
-            for i in range(len(sources)):
-                src = sources[i]
-                # custom
-                custom_args_string = ""
-                if i in custom_flags:
-                    for flag in custom_flags[i]:
-                        custom_args_string += flag + " "
-                if i in custom_definitions:
-                    for definition in custom_definitions[i]:
-                        if definition.find(r'=\"') != -1:
-                            lst = re.split(r'\\"', definition)
-                            if len(lst) > 2:
-                                # 为空格添加转义符号
-                                lst[1] = lst[1].replace(" ", "\ ")
-                                definition = lst[0] + r"\"" + lst[1] + r"\""
-                            else:
-                                self._logger.warning("custom definition %s analysis error" % definition)
-                        custom_args_string += "-D" + definition + " "
-
-                transfer_name = file_args_MD5Calc(src, \
-                                      job_dict["flags"], job_dict["definitions"], job_dict["includes"], custom_args_string)
-                output_command = self.compile_command[job_dict["compiler_type"]] + " "
-                output_command += args_string
-                output_command += custom_args_string
->>>>>>> develop
                 output_command += "-c " + src + " "
                 output_command += "-o " + self.output_path + "/" + transfer_name + ".o "
 
@@ -161,36 +115,14 @@ class CommandExec(building_process.ProcessBuilder):
 
             p = subprocess.Popen(cmd, shell=True, \
                                  stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-<<<<<<< HEAD
             retval = p.wait()
             for line in p.stdout.readlines():
                sys.stdout.write(line)
-=======
-            # 可能会导致管道阻塞
-            """
-            retval = p.wait()
-            sys.stdout.write("------ " + file + " --------\n" + p.stdout.read())
->>>>>>> develop
 
             if retval == 0:
                 self._logger.info("compile: %s success" % file)
             else:
                 self._logger.warning("compile: %s fail" % file)
-<<<<<<< HEAD
-=======
-            """
-            # 推荐做法
-            out, err = p.communicate()
-            if out:
-                sys.stdout.write("------ " + file + " --------\n" + out)
-            else:
-                sys.stdout.write("------ " + file + " --------\n")
-
-            if p.returncode != 0:
-                self._logger.warning("compile: %s fail" % file)
-            else:
-                self._logger.info("compile: %s success" % file)
->>>>>>> develop
 
             lock.acquire()
         lock.release()
@@ -242,21 +174,13 @@ def file_transfer(filename, path):
     return fileMD5Calc(file)
 
 
-<<<<<<< HEAD
 def file_args_MD5Calc(file, flags, definitions, includes):
-=======
-def file_args_MD5Calc(file, flags, definitions, includes, custom_arg):
->>>>>>> develop
     """
     文件可能被多次编译，因此需要用编译参数加到一起来标识一个目标
     :param file:
     :param flags:
     :param definitions:
     :param includes:
-<<<<<<< HEAD
-=======
-    :param custom_arg:
->>>>>>> develop
     :return:
     """
     m = hashlib.md5()
@@ -267,11 +191,6 @@ def file_args_MD5Calc(file, flags, definitions, includes, custom_arg):
         configs.sort()
         for line in configs:
             m.update(line)
-<<<<<<< HEAD
-=======
-    if custom_arg:
-        m.update(custom_arg)
->>>>>>> develop
     return m.hexdigest()
 
 
@@ -379,16 +298,9 @@ class CaptureBuilder(object):
                  output_path=None,
                  build_type="cmake",
                  build_path=None,
-<<<<<<< HEAD
                  compiler_type=DEFAULT_COMPILE_COMMAND, \
                  compiler_path=None):
         self.__prefers = []
-=======
-                 prefers = [],
-                 compiler_type=DEFAULT_COMPILE_COMMAND, \
-                 compiler_path=None):
-        self.__prefers = prefers
->>>>>>> develop
         self.__root_path = os.path.abspath(root_path)
         self.compiler_type = compiler_type
         self.__build_type = build_type
@@ -440,68 +352,8 @@ class CaptureBuilder(object):
         if self.__build_type == "cmake":
             if not self.__build_path:
                 self.__build_path = self.__root_path + "/build"
-<<<<<<< HEAD
             source_infos, include_files, files_count= \
                source_detective.get_present_path_cmake(self.__root_path, self.__prefers, self.__build_path)
-=======
-            source_infos, include_files, files_count = \
-               source_detective.get_present_path_cmake(self.__root_path, self.__prefers, self.__build_path)
-        elif self.__build_type == "make":   # TODO: 添加普通项目的Makefile解析
-            sub_paths, files_s, files_h, files_s_defs = \
-                source_detective.get_present_path_autotools(self.__root_path, self.__prefers)
-            gcc_string = "gcc"
-
-            # is_has_configure = os.path.exists(self.__root_path + "/configure")
-            # if is_has_configure:
-            gcc_string += " -DHAVE_CONFIG_H"
-
-            gcc_include_string = ""
-            for path in sub_paths:
-                gcc_include_string = gcc_include_string + " " + "-I" + path
-
-            source_files = [x[0] for x in files_s]
-            commands = []
-            with open(self.__output_path + "/" + "my_compile.sh", "w+") as fout:
-                for source_file_tuple, definition in zip(files_s, files_s_defs):
-                    source_file = source_file_tuple[0]
-                    suffix = source_file.split(".")[-1]
-                    index = -(len(suffix))
-                    file_name = source_file.split("/")[-1]
-                    file_index = -(len(file_name) + 1)
-                    output_path_str = self.__output_path
-                    if not output_path_str:
-                        output_path_str = source_file[:file_index]
-                    else:
-                        # 设置输出目录
-                        output_path_str = output_path_str + "/fileCache"
-                        if not os.path.exists(output_path_str):
-                            os.makedirs(output_path_str)
-                    output_file_path = output_path_str + "/" + source_file_tuple[1] + "_" + file_name[:index] + "o"
-                    makestring = gcc_string + definition + " -c " + source_file + " -o " + output_file_path + gcc_include_string
-                    fout.write(makestring + "\n")
-                    commands.append(makestring)
-            commands_dump(self.__output_path + "/compile_commands.json",
-                          source_files,
-                          commands,
-                          [self.__output_path for i in source_files]
-                        )
-            return [], [], []
-
-            pass
-        else:   # 连Makefile都没有，直接构建
-            """
-            TODO: 扫描用Makefile直接构建的项目
-                1. 先获取所有的源文件, 头文件
-                2. 根据源文件去获取对应的目标文件是怎产生的,
-                    (1) make -d -n -k, 其中-n是为不实际执行make, 然后将输出的文本进行检索,构建出所依赖的变量
-                    (2) 写入伪目标到Makefile中, 伪目标的操作主要是打印我们需要的参数值
-                    (3) 获取返回值, 解析这些返回值
-                3. 根据截获的参数构建出源文件的编译命令
-            """
-            paths, files_s, files_h = \
-                source_detective.get_present_path_make(self.__root_path, self.__prefers)
-            pass
->>>>>>> develop
         self._logger.info("End of Scaning project folders...")
 
         # Test
@@ -509,25 +361,17 @@ class CaptureBuilder(object):
         json.dump(source_infos, fout, indent=4)
         return source_infos, include_files, files_count
 
-<<<<<<< HEAD
     def command_prebuild(self, source_infos, files_count):
-=======
-    def command_prebuild(self, source_infos, files_count, compiler_id="GNU"):
->>>>>>> develop
         # if len(source_infos) > 100 and files_count > 5000:
             # 只有任务比较多的时候，构建才需要并行化
         command_builder = CommandBuilder(self._logger)
         command_builder.distribute_jobs(source_infos)
         # setting
-<<<<<<< HEAD
         compiler_command_map = {
             "CXX": "g++",
             "C": "gcc"
         }
         command_builder.basic_setting(compiler_command_map, self.__output_path)
-=======
-        command_builder.basic_setting(COMPILER_COMMAND_MAP[compiler_id], self.__output_path)
->>>>>>> develop
         command_builder.redis_setting()
         result_list = command_builder.run()
         fout = open("result.json", "w")
@@ -547,23 +391,14 @@ class CaptureBuilder(object):
 
 def get_system_compiler_path(compiler_type):
     """获取系统自带的编译器路径"""
-<<<<<<< HEAD
-=======
-    a = 10
->>>>>>> develop
     pass
 
 
 def parse_prefer_str(prefer_str, input_path):
     if prefer_str == "":
         prefers = []
-<<<<<<< HEAD
     if prefer_str == "all":
         prefers = source_detective.get_dir(input_path)
-=======
-    elif prefer_str == "all":
-        prefers = source_detective.get_directions(input_path)
->>>>>>> develop
     else:
         prefers = prefer_str.strip(' \n\t').split(",")
     return prefers
@@ -575,11 +410,6 @@ def main():
     output_path = ""
     prefers_str = ""
     make_type = "cmake"
-<<<<<<< HEAD
-=======
-    cmake_build_path = ""
-    compiler_id = None
->>>>>>> develop
     if len(sys.argv) == 2:
         input_path = sys.argv[1]
     elif len(sys.argv) == 3:
@@ -601,27 +431,11 @@ def main():
         output_path = sys.argv[3]
         make_type = sys.argv[4]
         cmake_build_path = sys.argv[5]
-<<<<<<< HEAD
-=======
-    elif len(sys.argv) == 7:
-        input_path = sys.argv[1]
-        prefers_str = sys.argv[2]
-        output_path = sys.argv[3]
-        make_type = sys.argv[4]
-        cmake_build_path = sys.argv[5]
-        compiler_id = sys.argv[6]
->>>>>>> develop
     else:
         sys.stderr.write(
     """Please input project root path to compiler.
     Usage:
-<<<<<<< HEAD
         python program_name root_path [prefer_sub_folder1,prefer_sub_folder2,...] [outer_output_path]
-=======
-        python program_name root_path [prefer_sub_folder1,prefer_sub_folder2,...] [outer_output_path] [build_type] [build_path] [compiler_id]
-        
-    compiler_id = ["GNU", "Clang"]
->>>>>>> develop
 """)
         sys.exit(-1)
 
@@ -643,22 +457,10 @@ def main():
 
     # CaptureBuilder
     capture_builder = CaptureBuilder(logger, input_path, output_path, \
-<<<<<<< HEAD
                                      build_type=make_type, build_path=cmake_build_path)
     source_infos, include_files, files_count = capture_builder.scan_project()
 
     capture_builder.command_prebuild(source_infos, files_count)
-=======
-                                     prefers=prefers, build_type=make_type, build_path=cmake_build_path)
-    source_infos, include_files, files_count = capture_builder.scan_project()
-
-    # 暂时不编译违背cmake 定义的源文件
-    if compiler_id:
-        if compiler_id not in COMPILER_COMMAND_MAP:
-            sys.stderr.write("No such compiler_id!")
-        capture_builder.command_prebuild(source_infos[:-2], files_count, compiler_id)
-    capture_builder.command_prebuild(source_infos[:-2], files_count)
->>>>>>> develop
 
 
 if __name__ == "__main__":
--- utils/parse_cmake.py
+++ utils/parse_cmake.py
@@ -71,28 +71,13 @@ def set_analysis(fin):
 
         for arg in args_result:
             if temp_key in config:
-<<<<<<< HEAD
                 config[temp_key].append(strip_quotation(arg))
-=======
-                arg = strip_quotation(arg)
-                check_equal = re.match("(.*)=(.*)", arg)
-                if check_equal:
-                    key = check_equal.group(1)
-                    value = check_equal.group(2)
-                    if re.match("[\"\'](.*)[\"\']", value):
-                        value = value.replace("\"", "\\\"")
-                    else:
-                        value = "\"" + value + "\""
-                    arg = key + "=" + value
-                config[temp_key].append(arg)
->>>>>>> develop
             else:
                 config[temp_key] = []
     return config
 
 
 def parse_flags(flags_file):
-<<<<<<< HEAD
     flags_set = set(["CXX_FLAGS", "C_FLAGS"])
     fin = open(flags_file, "r")
     data = ""
@@ -101,87 +86,29 @@ def parse_flags(flags_file):
         # 跳过注释和空行
         if len(line) == 0 or line[0] == '#':
             continue
-=======
-    """
-    解析cmake flags.make文件
-    :param flags_file:
-    :return: final_flags, custom_flags, custom_definitions
-    """
-    flags_set = ["CXX_FLAGS", "C_FLAGS"]
-    fin = open(flags_file, "r")
-    data = [None, None]
-    custom_definitions = {}
-    custom_flags = {}
-    for line in fin:
-        line = line.strip(' \t\n')
-        # 跳过注释和空行
-        if len(line) == 0:
-            continue
-        if line[0] == '#':
-            # 可能是注释行也可能是custom配置行
-            custom_flag = re.match(r"# Custom flags: (.*)_FLAGS = (.*)", line)
-            if custom_flag:
-                relative_file_path = custom_flag.group(1)
-                custom_flag_data = custom_flag.group(2)
-                file_custom_flags = re.split("\s+(?=-)", custom_flag_data)
-                custom_flags[relative_file_path] = file_custom_flags
-                continue
-
-            custom_definition = re.match("# Custom defines: (.*)_DEFINES = (.*)", line)
-            if custom_definition:
-                relative_file_path = custom_definition.group(1)
-                custom_definition_data = custom_definition.group(2)
-                file_custom_definitions = re.split(";", custom_definition_data)
-                custom_definitions[relative_file_path] = file_custom_definitions
-                continue
->>>>>>> develop
         # 获取带comment的行， 并去除行尾的注释
         setting_line_with_comment = re.match(r".+#\S*", line)
         if setting_line_with_comment:
             line = re.split("\s+\#", line)[0]
 
-<<<<<<< HEAD
         lst = re.split(r"\s+=\s+", line)
         if lst[0] in flags_set:
             data = lst[1]
             break
     flags = re.split("\s+(?=-)", data)
     return flags
-=======
-        lst = re.split(r"\s+=\s*", line)
-        if lst[0] == flags_set[0]:
-            data[0] = lst[1]
-        elif lst[0] == flags_set[1]:
-            data[1] = lst[1]
-    final_flags = []
-    if data[0] is not None and data[1] is not None:
-        for flag in data:
-            flags = re.split("\s+(?=-)", flag)
-            final_flags.append(flags)
-    elif data[1] is None:
-        flags = re.split("\s+(?=-)", data[0])
-        final_flags = [flags,]
-    elif data[0] is None:
-        flags = re.split("\s+(?=-)", data[1])
-        final_flags = [flags,]
-    return final_flags, custom_flags, custom_definitions
->>>>>>> develop
 
 
 def parse_cmakeInfo(depen_file):
     """
     解析DependInfo.cmake文件
-<<<<<<< HEAD
     TODO: 这里返回的include路径是相对路径，因此，我的代码需要改成跳转到指定目录下执行
-=======
->>>>>>> develop
     :param file:    DependInfo.cmake的文件路径
     :return: files_s, definitions, includes
     """
     fin = open(depen_file, 'r')
     config_dict = set_analysis(fin)
 
-<<<<<<< HEAD
     compiler_type = ""
     if type(config_dict["CMAKE_DEPENDS_LANGUAGES"]) == types.ListType:
         compiler_type = config_dict["CMAKE_DEPENDS_LANGUAGES"][0]
@@ -208,43 +135,4 @@ def parse_cmakeInfo(depen_file):
     files_s = filter(lambda file: file[-2:] != ".o", files)
 
     return files_s, definitions, includes, compiler_type
-=======
-    if type(config_dict["CMAKE_DEPENDS_LANGUAGES"]) == types.ListType:
-        compiler_type = config_dict["CMAKE_DEPENDS_LANGUAGES"]
-    else:
-        compiler_type = (config_dict["CMAKE_DEPENDS_LANGUAGES"],)
-
-    # 获取不同编译器的配置项
-    cmake_infos = []
-
-    if "CXX" in compiler_type:
-        source_field_cxx = "CMAKE_DEPENDS_CHECK_CXX"
-        definition_field_cxx = "CMAKE_TARGET_DEFINITIONS_CXX"
-        include_field_cxx = "CMAKE_CXX_TARGET_INCLUDE_PATH"
-        files_cxx, definitions_cxx, includes_cxx = [], [], []
-        if source_field_cxx in config_dict:
-            files_cxx = config_dict[source_field_cxx]
-            # files_cxx = filter(lambda file: file[-2:] != ".o", files_cxx)
-        if definition_field_cxx in config_dict:
-            definitions_cxx = config_dict[definition_field_cxx]
-        if include_field_cxx in config_dict:
-            includes_cxx = config_dict[include_field_cxx]
-        cmake_infos.append([files_cxx, definitions_cxx, includes_cxx, "CXX"])
-
-    if "C" in compiler_type:
-        source_field_c = "CMAKE_DEPENDS_CHECK_C"
-        definition_field_c = "CMAKE_TARGET_DEFINITIONS_C"
-        include_field_c = "CMAKE_C_TARGET_INCLUDE_PATH"
-        files_c, definitions_c, includes_c = [], [], []
-        if source_field_c in config_dict:
-            files_c = config_dict[source_field_c]
-            # files_c = filter(lambda file: file[-2:] != ".o", files_c)
-        if definition_field_c in config_dict:
-            definitions_c = config_dict[definition_field_c]
-        if include_field_c in config_dict:
-            includes_c = config_dict[include_field_c]
-        cmake_infos.append([files_c, definitions_c, includes_c, "C"])
-
-    return cmake_infos
->>>>>>> develop
 
