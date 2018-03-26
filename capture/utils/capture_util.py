d7166b3a282f038bea16525570599eaeef64b7b1
zengzhishi <zengzhishi@sbrella.com>
2018-03-26 20:45:31 +0800

    Update cmake_command_analyzer.py, and util

--- capture/utils/capture_util.py
+++ capture/utils/capture_util.py
@@ -11,6 +11,7 @@
 
 import subprocess
 import logging
+import re
 
 logger = logging.getLogger("capture")
 
@@ -67,6 +68,100 @@ def replace_escape(string):
         output_string = output_string.replace(key, value)
 
     return output_string
+
+
+def undefined_split(line, info_dict=None):
+    """
+    A split util function for cutting undefined line into pieces.
+    :param line:            A string contains of undefined var.
+    :param info_dict:       Checking dict for checking variable value.
+    :return:
+    """
+    if info_dict is None:
+        info_dict = dict()
+    dollar_var_pattern = r"(.*)\$\(([a-zA-Z_][a-zA-Z0-9_]*)\)(.*)"
+    at_var_pattern = r"(.*)\@([a-zA-Z_][a-zA-Z0-9_]*)\@(.*)"
+    with_var_line_regex = re.compile(dollar_var_pattern + r"|" + at_var_pattern)
+
+    with_var_line_match = with_var_line_regex.match(line)
+    # slices will be a reversed list
+    slices = []
+    while with_var_line_match:
+        match_chose = 0
+        if with_var_line_match.group(1) is None:
+            match_chose += 3
+
+        line = with_var_line_match.group(1 + match_chose)
+        undefine_var = with_var_line_match.group(2 + match_chose)
+        other = with_var_line_match.group(3 + match_chose)
+        if undefine_var in info_dict:
+            value = info_dict[undefine_var]
+            if len(value["undefined"]) == 0 and value["option"] is None:
+                undefine_var = " ".join(value["defined"]) if len(value["defined"]) != 0 else ""
+                line = line + undefine_var + other
+            elif value["option"] is None:
+                slices.append(other)
+                slices.append(value["defined"])
+                slices.append(value["undefined"])
+            else:
+                slices.append(other)
+                if match_chose == 0:
+                    slices.append("$({})".format(undefine_var))
+                else:
+                    slices.append("@{}@".format(undefine_var))
+        else:
+            slices.append(other)
+            if match_chose == 0:
+                slices.append("$({})".format(undefine_var))
+            else:
+                slices.append("@{}@".format(undefine_var))
+        with_var_line_match = with_var_line_regex.match(line)
+    slices.append(line)
+    slices.reverse()
+    return slices
+
+
+def unbalanced_quotes(s):
+    single = 0
+    double = 0
+    excute = 0
+    for c in s:
+        if c == "'":
+            single += 1
+        elif c == '"':
+            double += 1
+        if c == "`":
+            excute += 1
+
+    move_double = s.count('\\"')
+    move_single = s.count("\\'")
+    single -= move_single
+    double -= move_double
+
+    is_half_quote = single % 2 == 1 or double % 2 == 1 or excute % 2 == 1
+    return is_half_quote
+
+
+def split_line(line):
+    # Pass 1: split line using whitespace
+    words = line.strip().split()
+    # Pass 2: merge words so that the no. of quotes is balanced
+    res = []
+    for w in words:
+        if len(res) > 0 and unbalanced_quotes(res[-1]):
+            res[-1] += " " + w
+        else:
+            res.append(w)
+    return res
+
+
+def strip_quotes(s):
+    if s[0] == "'" and s[-1] == "'":
+        return s[1:-1]
+    elif s[0] == '"' and s[-1] == '"':
+        return s[1:-1]
+    else:
+        return s
 
 
 # Analysis Error happen
--- capture/utils/cmake_command_analyzer.py
+++ capture/utils/cmake_command_analyzer.py
@@ -9,16 +9,136 @@
     @Note: Define some cmake commands analyzer here.
 """
 import re
+import capture.utils.capture_util as capture_util
+from capture.utils.m4_macros_analysis import check_one_undefined_slice, check_undefined
+from capture.utils.m4_macros_analysis import check_one_undefined_slice_self, check_undefined_self
+
+# set_cache_type = ["BOOL", "FILEPATH", "PATH", "STRING", "INTERNAL"]
+
+var_pattern = r"[a-zA-Z_][a-zA-Z0-9_]*"
+env_pattern = r"ENV{" + var_pattern + r"}"
+docstring_pattern = r"\"[^\\\"]\""                 # exclude \"
+var_value_pattern = r"\$\(" + var_pattern + r"\)"
+
+value_without_quote_pattern = r"[^ \t\n\)]+"
+value_with_quote_pattern = docstring_pattern
+filename_flags = ["-I", "-isystem", "-iquote", "-include", "-imacros", "-isysroot"]
 
 
-def set_analyzer(match_args_line):
+# 1. Argument config command
+def set_analyzer(match_args_line, result, options, reverses):
+    var_dict = result.get("variable", dict())
+    list_var_dict = result.get("list_variable", dict())
+
+    # 1. variable
+    # TODO: consider whether we need a env_variable dict
+    variable_regex = re.compile("\s*(" + var_pattern + ")\s+(.*)")
+    variable_match = variable_regex.match(match_args_line)
+    env_regex = re.compile("\s*ENV{(" + var_pattern + ")}\s+(.*)")
+    env_match = env_regex.match(match_args_line)
+    if not variable_match and not env_match:
+        raise capture_util.ParserError("Set pattern error in set({}).".format(match_args_line))
+
+    if variable_match:
+        variable_name = variable_match.group(1)
+        match_line = variable_match.group(2)
+    else:
+        variable_name = env_match.group(1)
+        match_line = env_match.group(2)
+
+    # 2. value
+    words = capture_util.split_line(match_line)
+    if len(words) == 0:
+        # unset variable
+        pass
+    elif len(words) == 1 or len(words) > 1 and re.match(r"CACHE", words[1]):
+        # one elem config
+        value_line = words[0]
+        value_line = capture_util.strip_quotes(value_line)
+
+        temp = ""
+        values = capture_util.split_line(value_line)
+        for (i, word) in enumerate(values):
+            temp = temp + " " + word if temp else word
+            if i != len(values) - 1 and word in filename_flags and values[i + 1][0] != '-':
+                continue
+
+            value_with_quote_match = re.match(r"\"([^\\\"])\"", temp)
+            if value_with_quote_match:
+                value = value_with_quote_match.group(1)
+            else:
+                value = word
+            slices = capture_util.undefined_split(value, var_dict)
+
+            transfer_word = "".join(slices)
+            # TODO: 暂时无法表达 string 拼接操作
+            if check_undefined(slices, with_ac_var=False):
+                if check_undefined_self(slices, variable_name):
+                    if len(slices) == 1:
+                        var_dict["is_replace"] = False
+                    temp = ""
+                    continue
+                var_dict["undefined"].append(transfer_word)
+            else:
+                var_dict["defined"].append(transfer_word)
+            temp = ""
+    elif len(words) > 1 and not re.match(r"CACHE", words[1]):
+        # list config
+        value_list = list_var_dict.get("value", list())
+        for (i, value_line) in enumerate(words):
+            if re.match(r"CACHE", words[i]):
+                break
+            value_line = capture_util.strip_quotes(value_line)
+
+            temp = ""
+            values = capture_util.split_line(value_line)
+            var_dict = {
+                "defined": [],
+                "undefined": [],
+            }
+            for (i, word) in enumerate(values):
+                temp = temp + " " + word if temp else word
+                if i != len(values) - 1 and word in filename_flags and values[i + 1][0] != '-':
+                    continue
+
+                value_with_quote_match = re.match(r"\"([^\\\"])\"", temp)
+                if value_with_quote_match:
+                    value = value_with_quote_match.group(1)
+                else:
+                    value = word
+
+                slices = capture_util.undefined_split(value, var_dict)
+                transfer_word = "".join(slices)
+
+                # TODO: 暂时无法表达 string 拼接操作
+                if check_undefined(slices, with_ac_var=False):
+                    if check_undefined_self(slices, variable_name):
+                        if len(slices) == 1:
+                            var_dict["is_replace"] = False
+                        temp = ""
+                        continue
+                    var_dict["undefined"].append(transfer_word)
+                else:
+                    var_dict["defined"].append(transfer_word)
+                temp = ""
+        pass
+
+
+def list_analyzer(match_args_line, result, option, reverse):
     pass
 
 
-def list_analyzer(match_args_line):
+# 2. include
+def include_directories_analyzer(match_args_line, result, option, reverse):
     pass
 
 
+# other config command
+def set_target_properties_analyzer(match_args_line, result, option, reverse):
+    pass
 
+
+def transform_makefile_inc_analyzer(match_args_line, result, options, reverses):
+    pass
 
 # vi:set tw=0 ts=4 sw=4 nowrap fdm=indent
